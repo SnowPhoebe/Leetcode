6f8be39eca043705bb8767a393f3433edc9e1b42
SnowPhoebe <63526084+SnowPhoebe@users.noreply.github.com>
2022-10-12 10:09:55 -0400

    Time: 459 ms (91.89%), Space: 165.2 MB (70.21%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,15 +1,21 @@
 class LRUCache {
 public:
     LRUCache(int capacity) {
-        _capacity = capacity;
         size = 0;
+        _capacity = capacity;
     }
     
     int get(int key) {
+        // auto it = hash_table.find(key);
+        // if (it != hash_table.end()) {
+        //     lst.splice(lst.begin(), lst, it->second);
+        //     return it->second->second;
+        // }
+        // return -1;
         auto it = hash_table.find(key);
         if (it == hash_table.end()) return -1;
         else {
-            lru_list.splice(lru_list.begin(), lru_list, it->second);
+            lst.splice(lst.begin(), lst, it->second);
             return it->second->second;
         }
     }
@@ -18,25 +24,44 @@ public:
         auto it = hash_table.find(key);
         if (it != hash_table.end()) {
             it->second->second = value;
-            lru_list.splice(lru_list.begin(), lru_list, it->second);
+            lst.splice(lst.begin(), lst, it->second);
         }
         else if (size < _capacity) {
-            lru_list.push_front({key,value});
-            hash_table[key] = lru_list.begin();
+            lst.push_front({key,value});
+            hash_table[key] = lst.begin();
             ++size;
         }
         else if (size == _capacity){
-            hash_table.erase(lru_list.back().first);
-            lru_list.pop_back();
-            lru_list.push_front({key,value});
-            hash_table[key] = lru_list.begin();
+            hash_table.erase(lst.back().first);
+            lst.pop_back();
+            lst.push_front({key,value});
+            hash_table[key] = lst.begin();
         }
+        // auto it = hash_table.find(key);
+        // if (it != hash_table.end()) {
+        //     lst.splice(lst.begin(), lst, it->second);
+        //     it->second->second = value;
+        // }
+        // else {
+        //     if (size < _capacity) {
+        //         lst.push_front({key,value});
+        //         hash_table[key] = lst.begin();
+        //         ++size;
+        //     }
+        //     else if (size == _capacity) {
+        //         hash_table.erase(lst.back().first);
+        //         lst.pop_back();
+        //         lst.push_front({key,value});
+        //         hash_table[key] = lst.begin();
+        //     }
+        // }
     }
+    
 private:
     int _capacity;
     int size;
     unordered_map<int,list<pair<int,int>>::iterator> hash_table;
-    list<pair<int,int>> lru_list;
+    list<pair<int,int>> lst;
 };
 
 /**
