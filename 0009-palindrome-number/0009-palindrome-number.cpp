edc4acdb1d9205d528a7263ff0642ba11242037e
SnowPhoebe <63526084+SnowPhoebe@users.noreply.github.com>
2022-10-14 13:22:07 -0400

    Time: 20 ms (70.32%), Space: 5.7 MB (99.17%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        string original_str = str;
-        reverse(str.begin(), str.end());
-        return original_str == str;
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        // if (x >= 0 && x < 10) return true;
+        int sec_int = 0;
+        while (sec_int < x) {
+            int digit = x % 10;
+            x = x / 10;
+            sec_int = sec_int * 10 + digit;
+        }
+        
+        return (sec_int == x) || (sec_int/10 == x);
     }
 };
